60fa7074a631eb3ef8b76c3359e292673dee9863
zhengz9417 <zhengz9417@gmail.com>
2025-08-10 21:03:06 -0700

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+// firebase-init.js
+window.firebaseConfig = {
+  apiKey: "AIzaSyDuY7YE70MSO7kcjG6F7RyVK2NRQlNnViQ",
+  authDomain: "pokergame-10a72.firebaseapp.com",
+  projectId: "pokergame-10a72",
+  storageBucket: "pokergame-10a72.firebasestorage.app",
+  messagingSenderId: "622619303241",
+  appId: "1:622619303241:web:46d3bb03f01699d78a8e9",
+};
